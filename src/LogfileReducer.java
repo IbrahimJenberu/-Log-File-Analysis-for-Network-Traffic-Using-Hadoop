4ff2abde962faeefe925d2b2b6e8b83af4818807
Ibrahim Jenberu <152397228+IbrahimJenberu@users.noreply.github.com>
2025-01-26 22:29:28 -0800

    Create LogfileReducer.java

---
+++ src/LogfileReducer.java
@@ -0,0 +1,20 @@
+package com.example.hadoop;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class LogfileReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int sum = 0;
+        for (IntWritable val : values) {
+            sum += val.get();  // Sum the occurrences
+        }
+        result.set(sum);
+        context.write(key, result);  // Output: IP address and request count
+    }
+}
